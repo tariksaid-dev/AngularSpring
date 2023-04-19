b7bab82981ed2812094b5ac6562bc71e2abcfa77
tariksaid-dev <tariksaid.dev@gmail.com>
2023-04-19 18:55:05 +0200

    HTTP get a nuestro backend añadido

--- angular-proyect/src/app/clientes/cliente.service.ts
+++ angular-proyect/src/app/clientes/cliente.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { CLIENTES } from './clientes.json';
 import { Cliente } from './cliente';
-import { Observable, of } from 'rxjs';
+import { Observable, of, map } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 
 @Injectable({
@@ -9,10 +10,17 @@ import { Observable, of } from 'rxjs';
 })
 export class ClienteService {
 
-  constructor() { }
+  private urlEndPoint: string = 'http://localhost:8080/api/clientes';
+
+  constructor(private http: HttpClient) { }
 
   getClientes(): Observable<Cliente[]> {
-    return of(CLIENTES);
+    // return of(CLIENTES); Cuando queremos trabajar con data local
+    // return this.http.get<Cliente[]>(this.urlEndPoint); Forma 1, está bien, hacemos un casting con la clase que queremos que convierta los datos
+    return this.http.get(this.urlEndPoint).pipe(
+      map(response => response as Cliente[])
+    );
+    // forma 2, usando map, se hace un casting de la misma forma
   }
 
 }
