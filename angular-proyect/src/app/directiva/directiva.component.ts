3e6181feef0d111589075b39abb6f3f1e8ad3720
tariksaid-dev <tariksaid.dev@gmail.com>
2023-04-19 16:36:38 +0200

    Primera parte del front acabada

---
+++ angular-proyect/src/app/directiva/directiva.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-directiva',
+  templateUrl: './directiva.component.html',
+  styleUrls: ['./directiva.component.css']
+})
+export class DirectivaComponent {
+  listaCurso: string[] = ["TypeScript", "JavaScript", "Java", "PHP"];
+
+  habilitar: boolean = true;
+
+  setHabilitar(): void {
+    this.habilitar = (this.habilitar==true) ? false: true;
+  }
+}
